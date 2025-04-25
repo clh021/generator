277a10f4c36535f06da8024d832b9e659342d08d
chenlianghong <clh021@gmail.com>
2025-04-25 20:32:23 +0800

    Refactor: Combine version printing logic into main.go and remove old version.go file.

--- cmd/v1/main.go
+++ cmd/v1/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime/debug"
 	"strings"
 
 	"github.com/clh021/generator/pkg/config"
@@ -114,7 +115,32 @@ func printHelp() {
 	fmt.Println("  generator -quickstart                # 生成快速开始示例")
 	fmt.Println("  generator -dir /path/to/workdir      # 指定工作目录")
 	fmt.Println("  generator -template /path/to/templates -variables /path/to/variables -output /path/to/output") //
+}
 
+// 这些变量会在编译时通过 -ldflags 注入
+var (
+	Version    string // 版本号，例如 0.0.123
+	CommitID   string // Git Commit ID
+	CommitTime string // Git Commit Time
+	BuildTime  string // 构建时间
+)
+
+func printVersion() {
+	fmt.Println("Version:", Version)
+	fmt.Println("Commit ID:", CommitID)
+	fmt.Println("Commit Time:", CommitTime)
+	fmt.Println("Build Time:", BuildTime)
+
+	// 打印 Go 版本信息
+	if info, ok := debug.ReadBuildInfo(); ok {
+		fmt.Println("Go Version:", info.GoVersion)
+		fmt.Println("Go Modules:")
+		for _, dep := range info.Deps {
+			fmt.Printf("  %s %s\n", dep.Path, dep.Version)
+		}
+	} else {
+		fmt.Println("无法获取 Go 版本信息")
+	}
 }
 
 func generateQuickStartExample() error {
